3238182bef89d27ba5c2c66e70973c7bf2fb8dac
Jiri Dostal <jiri.dostal@honeywell.com>
2018-09-27 12:29:50 +0200

    Sdded check for background EP process. Bus signals for system objects working. Added better diagnostics for EP flags.

---
+++ mlep/core/writeSocketConfig.m
@@ -0,0 +1,27 @@
+function writeSocketConfig(fullFilePath,serverSocket, hostname)
+fid = fopen(fullFilePath, 'w');
+if fid == -1
+    % error
+    serverSocket.close;
+    error('Error while creating socket config file: %s', ferror(fid));
+end
+
+% Write socket config to file
+socket_config = [...
+    '<?xml version="1.0" encoding="ISO-8859-1"?>\n' ...
+    '<BCVTB-client>\n' ...
+    '<ipc>\n' ...
+    '<socket port="%d" hostname="%s"/>\n' ...
+    '</ipc>\n' ...
+    '</BCVTB-client>'];
+fprintf(fid, socket_config, serverSocket.getLocalPort, hostname);
+
+[femsg, ferr] = ferror(fid);
+if ferr ~= 0  % Error while writing config file
+    serverSocket.close; 
+    fclose(fid);
+    error('Error while writing socket config file: %s', femsg);
+end
+
+fclose(fid);
+end
